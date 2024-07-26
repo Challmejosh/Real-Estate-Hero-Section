c7f082f337a8c913bcc0119c2bcc53f48cd22df1
Adediran Joshua <challmejosh@gmail.com>
2024-07-26 02:13:38 -0700

    first commit

---
+++ src/Hero.js
@@ -0,0 +1,20 @@
+const Hero = () => {
+    return ( 
+        <div className="hero">
+            <div className="message">
+                <p>Find Your Dream <span>Home</span> Today</p>
+                <p>Discover a wide range of properties tailored to meet your lifestyle needs. Start your journey to homeownership with us.</p>
+            </div>
+            <div className="buttons">
+                <a href="/" className="btns">
+                    <p>See More</p>
+                </a>
+                <a href="/" className="btns">
+                    <p>Contact Us</p>
+                </a>
+            </div>
+        </div>
+     );
+}
+ 
+export default Hero;
